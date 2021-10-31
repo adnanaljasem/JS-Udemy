32d01a993064d4ae9def4fb8100a8e51282f149c
adnanaljasem~ <adnan.aljasem@powercoders.org>
2021-10-31 14:34:46 +0100

    .

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -154,26 +154,57 @@
 //   console.log("You have a friend called Adnan");
 // }
 
-const calcTip = function (bill) {
-  if (bill > 50 && bill < 300) {
-    tip = bill * 0.15;
-    return tip;
-  } else {
-    tip = bill * 0.2;
-    return tip;
-  }
+// const calcTip = function (bill) {
+//   if (bill > 50 && bill < 300) {
+//     tip = bill * 0.15;
+//     return tip;
+//   } else {
+//     tip = bill * 0.2;
+//     return tip;
+//   }
+// };
+// console.log(calcTip(401));
+
+// const bills = new Array(125, 555, 44);
+// console.log(bills);
+
+// // const tips = new Array(calcTip(125), calcTip(555), calcTip(44));
+// // console.log(tips);
+// const tipps = [];
+// tipps.push(calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2]));
+// console.log(tipps);
+// const total = [];
+
+// total.push(bills[0] + tipps[0], bills[1] + tipps[1], bills[2] + tipps[2]);
+// console.log(total);
+//objectsssssssssss:
+//the main difference: we accesss array with the index number . obj no! with key
+// how to get data from an object:
+// const adnan = {
+//   firstName: "Adnan",
+//   lastName: "Aljasem",
+//   age: 30,
+//   friends: ["Ahmad", "Tarek", "Mohammad"],
+// };
+// console.log(adnan);
+// const interestedIn = prompt("what do you want to know about Adnan?");
+// console.log(interestedIn);
+// if (adnan[interestedIn]) {
+//   console.log(adnan[interestedIn]);
+// } else {
+//   console.log("wrong request");
+// }
+// adnan.location = "ch";
+// adnan["hobby"] = "calligraphy";
+// console.log(adnan);
+
+// console.log(
+//   `${adnan.firstName} has ${adnan.friends.length} friends, and his best friend is called ${adnan.friends[0]}`
+// );
+//objects methods
+const adnan = {
+  firstName: "Adnan",
+  lastName: "Aljasem",
+  age: 30,
+  friends: ["Ahmad", "Tarek", "Mohammad"],
 };
-console.log(calcTip(401));
-
-const bills = new Array(125, 555, 44);
-console.log(bills);
-
-// const tips = new Array(calcTip(125), calcTip(555), calcTip(44));
-// console.log(tips);
-const tipps = [];
-tipps.push(calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2]));
-console.log(tipps);
-const total = [];
-
-total.push(bills[0] + tipps[0], bills[1] + tipps[1], bills[2] + tipps[2]);
-console.log(total);
